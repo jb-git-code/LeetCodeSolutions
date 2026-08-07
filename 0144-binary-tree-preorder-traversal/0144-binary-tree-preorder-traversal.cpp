91cddd01ef9dd66d19344a61e4a3df8816a1526e
Jayanshu Bhardwaj <jayanshub18@gmail.com>
2026-08-07 14:31:23 +0530

    Time: 0 ms (100%), Space: 11 MB (15.65%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -17,11 +17,27 @@ public:
         return res;
     }
 
-    void solve(TreeNode* root , vector<int> &res){
-        if(root == NULL) return ;
-        res.push_back(root -> val);
-        solve(root -> left , res);
-        solve(root -> right ,res);
-        return ;
+    void solve(TreeNode* node , vector<int> &res){
+        // if(root == NULL) return ;
+        // res.push_back(root -> val);
+        // solve(root -> left , res);
+        // solve(root -> right ,res);
+        // return ;
+        if(node == nullptr) {
+            return;
+        }
+        stack<TreeNode*> st;
+        st.push(node);
+        while(!st.empty()){
+            TreeNode* temp = st.top();
+            st.pop();
+            res.push_back(temp -> val);
+            if(temp -> right){
+                st.push(temp -> right);
+            }
+            if(temp -> left) {
+                st.push(temp -> left);
+            }
+        }
     }
 };
